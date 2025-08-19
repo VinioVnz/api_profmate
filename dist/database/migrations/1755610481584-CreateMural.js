24b52a556d2d6319fa091de6c8012d3fce900f40
vitor henkels <vitor.henkels@gmail.com>
2025-08-19 11:44:02 -0300

    migration de mural

---
+++ dist/database/migrations/1755610481584-CreateMural.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateMural1755610481584 = void 0;
+class CreateMural1755610481584 {
+    async up(queryRunner) {
+    }
+    async down(queryRunner) {
+    }
+}
+exports.CreateMural1755610481584 = CreateMural1755610481584;
