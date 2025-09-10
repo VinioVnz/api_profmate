abc6302c653bb4bcdb229009dd8ccb7aa5429171
Vinicius <vini.Stormtrooper@gmail.com>
2025-09-09 22:47:54 -0300

    alterementa consertado

--- src/database/migrations/1757440135866-AlterEmenta.ts
+++ src/database/migrations/1757440135866-AlterEmenta.ts
@@ -1,47 +1,40 @@
-import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
 
 export class AlterEmenta1757440135866 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Adiciona a coluna aluno_id
+    await queryRunner.addColumn(
+      "ementas",
+      new TableColumn({
+        name: "aluno_id",
+        type: "int",
+        isNullable: false,
+      })
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(new Table({
-                name: "ementas",
-                columns: [
-                    {
-                        name: "id",
-                        type: "int",
-                        isPrimary: true,
-                        isGenerated: true,
-                        generationStrategy: "increment",
-                    },
-                    {
-                        name: "modulo",
-                        type: "varchar",
-                    },
-                    {
-                        name: "topico",
-                        type: "varchar",
-                    },
-                    {
-                        name: "descricao",
-                        type: "varchar",
-                    },
-                    {
-                        name: "concluido",
-                        type: "boolean",
-                        default: false,
-                    },
-    
-                ],
-            }));
-            await queryRunner.createForeignKey("ementas", new TableForeignKey({
-                        columnNames: ["aluno_id"],
-                        referencedTableName: "alunos",
-                        referencedColumnNames: ["id"],
-                        onDelete: "CASCADE",
-                    }));
-        }
+    // Cria a foreign key para alunos(id)
+    await queryRunner.createForeignKey(
+      "ementas",
+      new TableForeignKey({
+        columnNames: ["aluno_id"],
+        referencedTableName: "alunos",
+        referencedColumnNames: ["id"],
+        onDelete: "CASCADE",
+      })
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Remove a FK
+    const table = await queryRunner.getTable("ementas");
+    const foreignKey = table!.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf("aluno_id") !== -1
+    );
+    if (foreignKey) {
+      await queryRunner.dropForeignKey("ementas", foreignKey);
     }
 
+    // Remove a coluna
+    await queryRunner.dropColumn("ementas", "aluno_id");
+  }
 }
